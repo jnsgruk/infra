100b574196df1ece580ae918ad0060543a096912
Jon Seager <jonseager@gmail.com>
2021-01-13 10:07:58 +0000

    Added Fastmail config for sgrs.uk

--- terraform/dns-sgrs-uk.tf
+++ terraform/dns-sgrs-uk.tf
@@ -29,56 +29,56 @@ resource "digitalocean_record" "sgrsuk-ns3" {
 
 # CNAME Records
 
-// resource "digitalocean_record" "sgrsuk-dkim1" {
-//   domain = digitalocean_domain.sgrsuk.id
-//   type   = "CNAME"
-//   name   = "fm1._domainkey"
-//   value  = "fm1.sgrs.uk.dkim.fmhosted.com."
-//   ttl    = 3600
-// }
+resource "digitalocean_record" "sgrsuk-dkim1" {
+  domain = digitalocean_domain.sgrsuk.id
+  type   = "CNAME"
+  name   = "fm1._domainkey"
+  value  = "fm1.sgrs.uk.dkim.fmhosted.com."
+  ttl    = 3600
+}
 
-// resource "digitalocean_record" "sgrsuk-dkim2" {
-//   domain = digitalocean_domain.sgrsuk.id
-//   type   = "CNAME"
-//   name   = "fm2._domainkey"
-//   value  = "fm2.sgrs.uk.dkim.fmhosted.com."
-//   ttl    = 3600
-// }
+resource "digitalocean_record" "sgrsuk-dkim2" {
+  domain = digitalocean_domain.sgrsuk.id
+  type   = "CNAME"
+  name   = "fm2._domainkey"
+  value  = "fm2.sgrs.uk.dkim.fmhosted.com."
+  ttl    = 3600
+}
 
-// resource "digitalocean_record" "sgrsuk-dkim3" {
-//   domain = digitalocean_domain.sgrsuk.id
-//   type   = "CNAME"
-//   name   = "fm3._domainkey"
-//   value  = "fm3.sgrs.uk.dkim.fmhosted.com."
-//   ttl    = 3600
-// }
+resource "digitalocean_record" "sgrsuk-dkim3" {
+  domain = digitalocean_domain.sgrsuk.id
+  type   = "CNAME"
+  name   = "fm3._domainkey"
+  value  = "fm3.sgrs.uk.dkim.fmhosted.com."
+  ttl    = 3600
+}
 
 # TXT Records
 
-// resource "digitalocean_record" "sgrsuk-fastmail-spf" {
-//   domain = digitalocean_domain.sgrsuk.id
-//   type   = "TXT"
-//   name   = "@"
-//   value  = "v=spf1 include:spf.messagingengine.com ?all"
-//   ttl    = 3600
-// }
+resource "digitalocean_record" "sgrsuk-fastmail-spf" {
+  domain = digitalocean_domain.sgrsuk.id
+  type   = "TXT"
+  name   = "@"
+  value  = "v=spf1 include:spf.messagingengine.com ?all"
+  ttl    = 3600
+}
 
 # MX Records
 
-// resource "digitalocean_record" "sgrsuk-fastmail-1" {
-//   domain   = digitalocean_domain.sgrsuk.id
-//   type     = "MX"
-//   name     = "@"
-//   value    = "in1-smtp.messagingengine.com."
-//   priority = 10
-//   ttl      = 3600
-// }
+resource "digitalocean_record" "sgrsuk-fastmail-1" {
+  domain   = digitalocean_domain.sgrsuk.id
+  type     = "MX"
+  name     = "@"
+  value    = "in1-smtp.messagingengine.com."
+  priority = 10
+  ttl      = 3600
+}
 
-// resource "digitalocean_record" "sgrsuk-fastmail-2" {
-//   domain   = digitalocean_domain.sgrsuk.id
-//   type     = "MX"
-//   name     = "@"
-//   value    = "in2-smtp.messagingengine.com."
-//   priority = 20
-//   ttl      = 3600
-// }
+resource "digitalocean_record" "sgrsuk-fastmail-2" {
+  domain   = digitalocean_domain.sgrsuk.id
+  type     = "MX"
+  name     = "@"
+  value    = "in2-smtp.messagingengine.com."
+  priority = 20
+  ttl      = 3600
+}
