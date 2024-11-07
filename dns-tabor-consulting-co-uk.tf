238f14ad3bce42415ac677b546d4ef282030d12e
Jon Seager <jon@sgrs.uk>
2024-11-07 20:06:28 +0000

    dns: add A/AAAA for tabor-consulting.co.uk

--- dns-tabor-consulting-co-uk.tf
+++ dns-tabor-consulting-co-uk.tf
@@ -26,3 +26,54 @@ resource "digitalocean_record" "taborconsulting-ns3" {
   ttl    = 1800
 }
 
+# A Records
+
+resource "digitalocean_record" "taborconsulting-apex" {
+  domain = digitalocean_domain.taborconsulting.id
+  type   = "A"
+  name   = "@"
+  value  = "66.241.125.53"
+  ttl    = 3600
+}
+
+resource "digitalocean_record" "taborconsulting-www" {
+  domain = digitalocean_domain.taborconsulting.id
+  type   = "A"
+  name   = "www"
+  value  = "66.241.125.53"
+  ttl    = 3600
+}
+
+# AAAA
+resource "digitalocean_record" "taborconsulting-fly-aaaa" {
+  domain = digitalocean_domain.taborconsulting.id
+  type   = "AAAA"
+  name   = "@"
+  value  = "2a09:8280:1::4e:a275:0"
+  ttl    = 3600
+}
+
+resource "digitalocean_record" "taborconsulting-fly-aaaa-www" {
+  domain = digitalocean_domain.taborconsulting.id
+  type   = "AAAA"
+  name   = "@"
+  value  = "2a09:8280:1::4e:a275:0"
+  ttl    = 3600
+}
+
+resource "digitalocean_record" "taborconsulting-fly-cert-validation-apex" {
+  domain = digitalocean_domain.taborconsulting.id
+  type   = "CNAME"
+  name   = "_acme-challenge"
+  value  = "tabor-consulting.co.uk.wp9qy8.flydns.net."
+  ttl    = 3600
+}
+
+resource "digitalocean_record" "taborconsulting-fly-cert-validation-www" {
+  domain = digitalocean_domain.taborconsulting.id
+  type   = "CNAME"
+  name   = "_acme-challenge.www"
+  value  = "www.tabor-consulting.co.uk.wp9qy8.flydns.net."
+  ttl    = 3600
+}
+
